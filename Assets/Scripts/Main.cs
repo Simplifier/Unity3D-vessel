4d0598a1720188b7d454954d002ce0881e1f4027
alex <iseldomused@gmail.com>
2015-08-26 02:21:22 +0300

    upd

--- Assets/Scripts/Main.cs
+++ Assets/Scripts/Main.cs
@@ -15,39 +15,39 @@ public class Main : MonoBehaviour {
 	private readonly Inputs input = new Inputs(0, .034f, false);
 	private Material material;
 	private const float pixelsPerUnit = 50;
-    private Stack<Mesh> meshPool = new Stack<Mesh>(); 
+	private Stack<Mesh> meshPool = new Stack<Mesh>();
 
-	void Start () {
+	private void Start() {
 		defaultGame = getDefaultGame();
-		createMaterial ();
+		createMaterial();
 	}
 
-	private void createMaterial(){
+	private void createMaterial() {
 		// Unity has a built-in shader that is useful for drawing
 		// simple colored things.
-		var shader = Shader.Find ("Hidden/Internal-Colored");
-	    material = new Material(shader) {hideFlags = HideFlags.HideAndDontSave};
-	    // Turn on alpha blending
-		material.SetInt ("_SrcBlend", (int)BlendMode.SrcAlpha);
-		material.SetInt ("_DstBlend", (int)BlendMode.OneMinusSrcAlpha);
+		var shader = Shader.Find("Hidden/Internal-Colored");
+		material = new Material(shader) {hideFlags = HideFlags.HideAndDontSave};
+		// Turn on alpha blending
+		material.SetInt("_SrcBlend", (int)BlendMode.SrcAlpha);
+		material.SetInt("_DstBlend", (int)BlendMode.OneMinusSrcAlpha);
 		// Turn backface culling off
-		material.SetInt ("_Cull", (int)CullMode.Off);
+		material.SetInt("_Cull", (int)CullMode.Off);
 		// Turn off depth writes
-		material.SetInt ("_ZWrite", 0);
-		material.SetPass (0);
+		material.SetInt("_ZWrite", 0);
+		material.SetPass(0);
 	}
 
-	void Update() {
-	    var left = Input.GetKey(KeyCode.LeftArrow);
-        var right = Input.GetKey(KeyCode.RightArrow);
-	    input.dir = 0;
-        if(left && !right){
-            input.dir = -1;
-        } else if (!left && right) {
-            input.dir = 1;
-        }
-        input.space = Input.GetKey(KeyCode.Space);
-	    input.delta = Time.deltaTime;
+	private void Update() {
+		var left = Input.GetKey(KeyCode.LeftArrow);
+		var right = Input.GetKey(KeyCode.RightArrow);
+		input.dir = 0;
+		if (left && !right) {
+			input.dir = -1;
+		} else if (!left && right) {
+			input.dir = 1;
+		}
+		input.space = Input.GetKey(KeyCode.Space);
+		input.delta = Time.deltaTime;
 
 		defaultGame = stepStart(input, defaultGame);
 		display(defaultGame);
@@ -59,13 +59,13 @@ public class Main : MonoBehaviour {
 			pieces.Add(new Piece(curve(n, 20), (n + 30) * 10, 0, -startSpeed, startWidth, startPieceH));
 		}
 
-		return new Game{
-		    cnt = 0,
-		    score = 0,
-		    ship = new Ship(0, shipStartY, 0, 0),
-		    t = new Tunnel(startWidth, startSpeed, curve(0, 20), 300, startPieceH, 20),
-		    debri = new List<Debri>(),
-		    state = State.Waiting,
+		return new Game {
+			cnt = 0,
+			score = 0,
+			ship = new Ship(0, shipStartY, 0, 0),
+			t = new Tunnel(startWidth, startSpeed, curve(0, 20), 300, startPieceH, 20),
+			debri = new List<Debri>(),
+			state = State.Waiting,
 			pieces = pieces
 		};
 	}
@@ -138,10 +138,10 @@ public class Main : MonoBehaviour {
 		return game.pieces;
 	}
 
-	private  List<Piece> stepPieces(float t, Game game) {
+	private List<Piece> stepPieces(float t, Game game) {
 		return addPiece(game)
-            .Select(piece => piece.stepObj(t) as Piece)
-            .Where(piece => piece.filter()).ToList();
+			.Select(piece => piece.stepObj(t) as Piece)
+			.Where(piece => piece.filter()).ToList();
 	}
 
 	private Debri addD(float sx, float sy, int n) {
@@ -160,15 +160,15 @@ public class Main : MonoBehaviour {
 			}
 		} else {
 			return debri.Select(debris => {
-			    debris.deg = (debris.deg + 20) % 360;
-                return debris;
+				debris.deg = (debris.deg + 20) % 360;
+				return debris;
 			}).ToList();
 		}
 		return debri;
 	}
 
-	private List<Debri> stepDebri(float t, Ship ship, List<Debri> debri){
-	    return addDebri(ship, debri).Select(debris => debris.stepObj(t) as Debri).ToList();
+	private List<Debri> stepDebri(float t, Ship ship, List<Debri> debri) {
+		return addDebri(ship, debri).Select(debris => debris.stepObj(t) as Debri).ToList();
 	}
 
 	private bool inside(Ship ship, Piece piece) {
@@ -176,7 +176,7 @@ public class Main : MonoBehaviour {
 	}
 
 	private State updateState(Game game) {
-	    var pieces = game.pieces.Where(p => withinN(60, p.y, game.ship.y));
+		var pieces = game.pieces.Where(p => withinN(60, p.y, game.ship.y));
 		return pieces.Any(piece => inside(game.ship, piece)) ? State.Playing : State.Dead;
 	}
 
@@ -191,16 +191,16 @@ public class Main : MonoBehaviour {
 		return s;
 	}
 
-	private Game stepDead(Inputs input, Game game){
+	private Game stepDead(Inputs input, Game game) {
 		if (input.space) {
 			defaultGame = getDefaultGame();
 			defaultGame.state = State.Playing;
 			return defaultGame;
 		}
 		if (game.cnt > game.score + 200) {
-		    defaultGame = getDefaultGame();
-		    defaultGame.state = State.Waiting;
-		    return defaultGame;
+			defaultGame = getDefaultGame();
+			defaultGame.state = State.Waiting;
+			return defaultGame;
 		}
 		game.debri = stepDebri(input.delta, game.ship, game.debri);
 		game.ship = hideShip(game.ship);
@@ -214,11 +214,11 @@ public class Main : MonoBehaviour {
 			defaultGame.state = State.Playing;
 			return defaultGame;
 		}
-	    game.pieces = stepPieces(input.delta, game);
-	    game.ship = autoShip(game.t, game.ship);
-	    game.t = updateTunnel(game);
-	    game.cnt++;
-	    return game;
+		game.pieces = stepPieces(input.delta, game);
+		game.ship = autoShip(game.t, game.ship);
+		game.t = updateTunnel(game);
+		game.cnt++;
+		return game;
 	}
 
 	private Game stepGame(Inputs input, Game game) {
@@ -232,7 +232,7 @@ public class Main : MonoBehaviour {
 	}
 
 	private Game stepStart(Inputs input, Game game) {
-		switch(game.state) {
+		switch (game.state) {
 			case State.Playing:
 				return stepGame(input, game);
 			case State.Dead:
@@ -242,39 +242,39 @@ public class Main : MonoBehaviour {
 		}
 		return null;
 	}
-	
+
 	//DISPLAY
 	private void drawPiece(Piece piece) {
-	    Color color;
-        Color.TryParseHexString("#ef2929", out color);
-	    var mesh = meshPool.Count > 0 ? meshPool.Pop() : new Mesh();
-	    mesh.vertices = new[] {
-	        new Vector3(-piece.width/2/pixelsPerUnit, -piece.height/2/pixelsPerUnit, 0),
-	        new Vector3(piece.width/2/pixelsPerUnit, -piece.height/2/pixelsPerUnit, 0),
-	        new Vector3(piece.width/2/pixelsPerUnit, piece.height/2/pixelsPerUnit, 0),
-	        new Vector3(-piece.width/2/pixelsPerUnit, piece.height/2/pixelsPerUnit, 0)
-	    };
-	    mesh.triangles = new[] {0, 1, 3, 1, 2, 3};
-	    mesh.colors = new[] {color, color, color, color};
+		Color color;
+		Color.TryParseHexString("#ef2929", out color);
+		var mesh = meshPool.Count > 0 ? meshPool.Pop() : new Mesh();
+		mesh.vertices = new[] {
+			new Vector3(-piece.width / 2 / pixelsPerUnit, -piece.height / 2 / pixelsPerUnit, 0),
+			new Vector3(piece.width / 2 / pixelsPerUnit, -piece.height / 2 / pixelsPerUnit, 0),
+			new Vector3(piece.width / 2 / pixelsPerUnit, piece.height / 2 / pixelsPerUnit, 0),
+			new Vector3(-piece.width / 2 / pixelsPerUnit, piece.height / 2 / pixelsPerUnit, 0)
+		};
+		mesh.triangles = new[] {0, 1, 3, 1, 2, 3};
+		mesh.colors = new[] {color, color, color, color};
 
-		Graphics.DrawMesh (mesh, new Vector3(piece.x / pixelsPerUnit, piece.y / pixelsPerUnit, 0), Quaternion.identity, material, 0);
+		Graphics.DrawMesh(mesh, new Vector3(piece.x / pixelsPerUnit, piece.y / pixelsPerUnit, 0), Quaternion.identity, material, 0);
 	}
 
 	private void drawDebri(Debri d) {
-	    var mesh = meshPool.Count > 0 ? meshPool.Pop() : new Mesh();
-        mesh.Clear();
-	    mesh.vertices = new[] {
-	        new Vector3(-5/pixelsPerUnit, -4.33f/pixelsPerUnit, 0),
-	        new Vector3(5/pixelsPerUnit, -4.33f/pixelsPerUnit, 0),
-	        new Vector3(0, 4.33f/pixelsPerUnit, 0)
-	    };
-	    mesh.triangles = new[] {0, 1, 2};
-	    mesh.colors = new[] {Color.white, Color.white, Color.white};
+		var mesh = meshPool.Count > 0 ? meshPool.Pop() : new Mesh();
+		mesh.Clear();
+		mesh.vertices = new[] {
+			new Vector3(-5 / pixelsPerUnit, -4.33f / pixelsPerUnit, 0),
+			new Vector3(5 / pixelsPerUnit, -4.33f / pixelsPerUnit, 0),
+			new Vector3(0, 4.33f / pixelsPerUnit, 0)
+		};
+		mesh.triangles = new[] {0, 1, 2};
+		mesh.colors = new[] {Color.white, Color.white, Color.white};
 
-	    var rotation = Quaternion.Euler(0, 0, d.deg);
-		Graphics.DrawMesh (mesh, new Vector3(d.x / pixelsPerUnit, d.y / pixelsPerUnit, 0), rotation, material, 0);
+		var rotation = Quaternion.Euler(0, 0, d.deg);
+		Graphics.DrawMesh(mesh, new Vector3(d.x / pixelsPerUnit, d.y / pixelsPerUnit, 0), rotation, material, 0);
 	}
-	
+
 	/*private Sprite txt(string t) {
 		var tf = new TextField;
 		tf.autoSize = TextFieldAutoSize.LEFT;
@@ -307,45 +307,46 @@ public class Main : MonoBehaviour {
 		}
 	}*/
 
-    private void drawShip(Ship ship){
-        var mesh = meshPool.Count > 0 ? meshPool.Pop() : new Mesh();
-        mesh.Clear();
-        mesh.vertices = new[] {
-            new Vector3(-10/pixelsPerUnit, -8.66f/pixelsPerUnit, 0),
-            new Vector3(10/pixelsPerUnit, -8.66f/pixelsPerUnit, 0),
-            new Vector3(0, 8.66f/pixelsPerUnit, 0)
-        };
-        mesh.triangles = new[] { 0, 1, 2 };
-        mesh.colors = new[] { Color.white, Color.white, Color.white };
+	private void drawShip(Ship ship) {
+		var mesh = meshPool.Count > 0 ? meshPool.Pop() : new Mesh();
+		mesh.Clear();
+		mesh.vertices = new[] {
+			new Vector3(-10 / pixelsPerUnit, -8.66f / pixelsPerUnit, 0),
+			new Vector3(10 / pixelsPerUnit, -8.66f / pixelsPerUnit, 0),
+			new Vector3(0, 8.66f / pixelsPerUnit, 0)
+		};
+		mesh.triangles = new[] {0, 1, 2};
+		mesh.colors = new[] {Color.white, Color.white, Color.white};
 
-	    Graphics.DrawMesh (mesh, new Vector3(ship.x / pixelsPerUnit, ship.y / pixelsPerUnit, 0), Quaternion.identity, material, 0);
+		Graphics.DrawMesh(mesh, new Vector3(ship.x / pixelsPerUnit, ship.y / pixelsPerUnit, 0), Quaternion.identity, material, 0);
 	}
-	
+
 	private void display(Game game) {
-	    meshPool = new Stack<Mesh>(FindObjectsOfType<Mesh>());
-		foreach(var piece in game.pieces) {
+		meshPool = new Stack<Mesh>(FindObjectsOfType<Mesh>());
+		foreach (var piece in game.pieces) {
 			drawPiece(piece);
 		}
 		drawShip(game.ship);
-		foreach(var debri in game.debri) {
+		foreach (var debri in game.debri) {
 			drawDebri(debri);
 		}
 		//displayText(game);
 	}
 }
 
-class Inputs {
+internal class Inputs {
 	public int dir;
 	public float delta;
 	public bool space;
-	public  Inputs(int dir, float delta, bool space) {
+
+	public Inputs(int dir, float delta, bool space) {
 		this.dir = dir;
 		this.delta = delta;
 		this.space = space;
 	}
 }
 
-class Tunnel {
+internal class Tunnel {
 	public float ampl;
 	public float h;
 	public float y;
@@ -353,7 +354,7 @@ class Tunnel {
 	public float speed;
 	public float width;
 
-	public  Tunnel(float width, float speed, float x, float y, float h, float ampl) {
+	public Tunnel(float width, float speed, float x, float y, float h, float ampl) {
 		this.ampl = ampl;
 		this.h = h;
 		this.y = y;
@@ -363,7 +364,7 @@ class Tunnel {
 	}
 }
 
-class GameObject {
+internal class GameObject {
 	public float vy;
 	public float vx;
 	public float y;
@@ -376,8 +377,8 @@ class GameObject {
 	}
 }
 
-class Ship : GameObject {
-	public  Ship(float x, float y, float vx, float vy) {
+internal class Ship : GameObject {
+	public Ship(float x, float y, float vx, float vy) {
 		this.vy = vy;
 		this.vx = vx;
 		this.y = y;
@@ -390,11 +391,11 @@ class Ship : GameObject {
 	}
 }
 
-class Piece : GameObject {
+internal class Piece : GameObject {
 	public float height;
 	public float width;
 
-	public  Piece(float x, float y, float vx, float vy, float width, float height) {
+	public Piece(float x, float y, float vx, float vy, float width, float height) {
 		this.height = height;
 		this.width = width;
 		this.vy = vy;
@@ -408,10 +409,10 @@ class Piece : GameObject {
 	}
 }
 
-class Debri : GameObject {
+internal class Debri : GameObject {
 	public int deg;
 
-	public  Debri(float x, float y, float vx, float vy, int deg) {
+	public Debri(float x, float y, float vx, float vy, int deg) {
 		this.deg = deg;
 		this.vy = vy;
 		this.vx = vx;
@@ -420,9 +421,9 @@ class Debri : GameObject {
 	}
 }
 
-class Game {
+internal class Game {
 	public int cnt;
-	public  List<Debri> debri;
+	public List<Debri> debri;
 	public Ship ship;
 	public Tunnel t;
 	public List<Piece> pieces;
@@ -430,8 +431,8 @@ class Game {
 	public State state;
 }
 
-enum State {
-    Waiting,
-    Playing,
-    Dead
+internal enum State {
+	Waiting,
+	Playing,
+	Dead
 };
